200cbe833e658fb3030fe407e394362e5278d1a7
Adasinho <adirm10@yahoo.com>
2020-07-13 21:35:52 +0200

    Updates

---
+++ precache-manifest.7d7e3a611c44104781b3dc2a648d279e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600f34f3b071f45673b93f104a04382e",
+    "url": "/photo-search-app/index.html"
+  },
+  {
+    "revision": "133e1bc19829dcda4295",
+    "url": "/photo-search-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d21b5d242624cb619654",
+    "url": "/photo-search-app/static/css/main.fe47fcb4.chunk.css"
+  },
+  {
+    "revision": "133e1bc19829dcda4295",
+    "url": "/photo-search-app/static/js/2.2d3ddb34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/photo-search-app/static/js/2.2d3ddb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21b5d242624cb619654",
+    "url": "/photo-search-app/static/js/main.fd4a2b4b.chunk.js"
+  },
+  {
+    "revision": "87f9e72aea1d56ffb71d",
+    "url": "/photo-search-app/static/js/runtime-main.b6428bef.js"
+  }
+]);
